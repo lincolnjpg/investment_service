5f2f0f8c05eadd68254f4a25488e2058eb9b9df7
Lincoln Junior <99145408+lincolnjpg@users.noreply.github.com>
2024-03-11 01:37:52 -0300

    refactor: adding constraint clauses

--- migrations/20240217233508_create_users_assets_fk.sql
+++ migrations/20240217233508_create_users_assets_fk.sql
@@ -1,8 +1,20 @@
 -- +goose Up
-ALTER TABLE users_assets ADD CONSTRAINT users_fk FOREIGN KEY(user_id) REFERENCES users (id);
-ALTER TABLE users_assets ADD CONSTRAINT assets_fk FOREIGN KEY(asset_id) REFERENCES assets (id);
+ALTER TABLE users_assets
+ADD CONSTRAINT users_fk FOREIGN KEY(user_id)
+REFERENCES users (id)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
+
+ALTER TABLE users_assets
+ADD CONSTRAINT assets_fk FOREIGN KEY(asset_id)
+REFERENCES assets (id)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
 
 -- +goose Down
-ALTER TABLE users_assets DROP CONSTRAINT users_fk;
-ALTER TABLE users_assets DROP CONSTRAINT assets_fk;
+ALTER TABLE users_assets
+DROP CONSTRAINT users_fk;
+
+ALTER TABLE users_assets
+DROP CONSTRAINT assets_fk;
 
